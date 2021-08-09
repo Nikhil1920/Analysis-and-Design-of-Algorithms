9d234373ff771a07d1c83a3119711d898a10b944
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-08-09 08:39:26 +0530

    add bear and steady gene problem

---
+++ skill/skill-2/bear-and-steady-gene.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class bearAndSteadyGene {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int j = n - 1;
+        sc.close();
+    }
+}
