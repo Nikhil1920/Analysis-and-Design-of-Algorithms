6758589ec624677b5b9d328397c27453079a4ebe
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-08-09 08:43:55 +0530

    add insertion sort part 2

---
+++ skill/skill-3/insertion-sort-part-2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class insertionSortPart2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(n, arr);
+        sc.close();
+    }
+
+    public static void sort(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+            printArray(arr);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
