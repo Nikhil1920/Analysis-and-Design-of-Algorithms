63c3ecc56ccca50814ffe23421ca15e9afee2d26
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-08-09 08:01:51 +0530

    refactored

--- practical/practical-1/inLab/sort-array-according-to-count-of-set-bits.java
+++ practical/practical-1/inLab/sort-array-according-to-count-of-set-bits.java
@@ -1,5 +1,4 @@
 class sortArrayAccordingToCountOfSetBits {
-    // Function to count the number of set bits
     static int countBits(int a) {
         int count = 0;
         while (a > 0) {
@@ -10,43 +9,30 @@ class sortArrayAccordingToCountOfSetBits {
         return count;
     }
 
-    static void insertionSort(int arr[], int aux[], int n) {
+    static void sortByCountOfSetBit(int arr[], int n) {
+        int setBitCount[] = new int[n];
+        for (int i = 0; i < n; i++)
+            setBitCount[i] = countBits(arr[i]);
         for (int i = 1; i < n; i++) {
-            // use 2 keys because we
-            // need to sort both
-            // arrays simultaneously
-            int key1 = aux[i];
-            int key2 = arr[i];
+            int bitCountKey = setBitCount[i];
+            int arrkey = arr[i];
             int j = i - 1;
 
-            while (j >= 0 && aux[j] < key1) {
-                aux[j + 1] = aux[j];
+            while (j >= 0 && setBitCount[j] < bitCountKey) {
+                setBitCount[j + 1] = setBitCount[j];
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
-            aux[j + 1] = key1;
-            arr[j + 1] = key2;
+            setBitCount[j + 1] = bitCountKey;
+            arr[j + 1] = arrkey;
         }
-    }
-
-    static void sortBySetBitCount(int arr[], int n) {
-        int aux[] = new int[n];
-        for (int i = 0; i < n; i++)
-            aux[i] = countBits(arr[i]);
-
-        insertionSort(arr, aux, n);
-    }
-
-    static void printArr(int arr[], int n) {
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
     }
 
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6 };
         int n = arr.length;
-        sortBySetBitCount(arr, n);
-        printArr(arr, n);
+        sortByCountOfSetBit(arr, n);
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
     }
-
 }
